00562c0946654e9515f9438b2cb6f9a15ca884cb
Omarkhaledkamal <khaldmer4@gmail.com>
2024-06-28 17:31:37 +0300

    The Game Is Finally Ready! , The Project Is DONE !
    Now the game is playable via the ui , you will
    need to refresh the page in order to play again
    first to 5 wins !

--- index.js
+++ index.js
@@ -18,10 +18,16 @@ function getComputerChoice() {
 let computerChoice = getComputerChoice();
 // done
 
-// get human choice
+let humanScoreText = document.querySelector(".score-container .human-score");
+let computerScoreText = document.querySelector(".score-container .computer-score");
 
+// get human choice
+let humanScore = 0;
+let computerScore = 0;
+let clickCounter = 0;
 const buttonContainer = document.querySelector(".button-container");
 buttonContainer.addEventListener("click", (e) => {
+  clickCounter++;
   let humanChoice, computerChoice = getComputerChoice();
   let target = e.target;
   switch (target.id) {
@@ -34,5 +40,47 @@ buttonContainer.addEventListener("click", (e) => {
     case 'scissors':
       humanChoice = c;
       break;
+  }
+  // add a paragraph to display details of round
+  const roundDetails = document.querySelector(".round-details");
+  if (humanChoice == computerChoice)
+    {
+      let drawPara = document.createElement("p");
+      drawPara.textContent = "Draw";
+      roundDetails.appendChild(drawPara);
+    }
+  else if (humanChoice == a && computerChoice == c || humanChoice == c && computerChoice == b  || humanChoice == b && computerChoice == a) {
+    let humanWinPara = document.createElement("p");
+    humanWinPara.textContent = `You Win! Round ${clickCounter}`;
+    roundDetails.appendChild(humanWinPara);
+    humanScoreText.textContent = `${++humanScore}`
+    console.log(humanChoice, computerChoice)
+  }
+  else {
+    let computerWinPara = document.createElement("p");
+    computerWinPara.textContent = `Computer Win! Round ${clickCounter}`;
+    roundDetails.appendChild(computerWinPara);
+    computerScoreText.textContent = `${++computerScore}`
+    console.log(humanChoice, computerChoice)
+  }
+
+  // make buttons refrence to stop game after winner has been declared
+  const buttons = document.querySelectorAll("button");
+  // declare winner (first to 5)
+  if (humanScore == 5 && computerScore < humanScore) {
+    let gameOverParaHuman = document.createElement("p");
+    gameOverParaHuman.textContent = `Game Over, You Win ! Refresh The Page To Play Again!`;
+    roundDetails.appendChild(gameOverParaHuman);
+    buttons.forEach((button) => {
+      button.disabled = true;
+    })
+  }
+  else if (computerScore == 5 && humanScore < computerScore) {
+    let gameOverParaComputer = document.createElement("p");
+    gameOverParaComputer.textContent = `Game Over, Computer Win ! Refresh The Page To Play Again!`;
+    roundDetails.appendChild(gameOverParaComputer);
+    buttons.forEach((button) => {
+      button.disabled = true;
+    })
   }
 })
